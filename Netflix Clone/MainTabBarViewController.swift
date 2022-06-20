7219288c006f4d1fb8c4ccb316f24a378c800524
Amin Bagheri <amin.bagheri@gmail.com>
2022-06-20 12:00:08 -0400

    Initial Commit

---
+++ Netflix Clone/MainTabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Amin  Bagheri  on 2022-06-20.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
